80d45c3598c6ab4cff9121b2672b1f8129af8c06
Tony Spiro <tspiro@tonyspiro.com>
2018-06-29 18:29:37 -0500

    first

---
+++ handler.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports.sendEmail = (event, context, callback) => {
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const body = JSON.parse(event.body)
+  const to = body.to;
+  const from = body.from;
+  const subject = body.subject;
+  const html_body = body.html_body;
+  const text_body = body.text_body;
+  const msg = {
+    to: to,
+    from: from,
+    subject: subject,
+    text: text_body,
+    html: html_body
+  };
+  sgMail.send(msg).then(data => {
+    // Celebrate
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+      },
+      body: JSON.stringify({
+        data: data
+      })
+    };
+    callback(null, response);
+  })
+  .catch(error => {
+    // Log friendly error
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+      },
+      body: JSON.stringify(error),
+    };
+    callback(null, response);
+  });
+};
